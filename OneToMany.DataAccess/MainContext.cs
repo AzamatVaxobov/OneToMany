130953b7e1c4a3aff56cc084705d58c5b21effd0
AzamatVaxobov <azamatvaxobov104@gmail.com>
2024-10-04 14:30:55 +0500

    ok

--- OneToMany.DataAccess/MainContext.cs
+++ OneToMany.DataAccess/MainContext.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OneToMany.DataAccess.Entites;
 
 namespace OneToMany.DataAccess;
 
 public class MainContext : DbContext
 {
+    public DbSet<Person> People { get; set; }
 }
